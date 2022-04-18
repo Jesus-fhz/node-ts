bac0a57d51c8f9acf5b2ba074c2f0c53ecfd6860
Jesus-fhz <jesusflores9325@gmail.com>
2022-04-18 13:49:48 +1000

    Learning about yup and how validatiosn against any schema works

--- src/middleware/validateRequest.ts
+++ src/middleware/validateRequest.ts
@@ -5,16 +5,16 @@ import { rest } from "lodash";
 
 //Currying Function
 export const validateRequest =  (schema: AnySchema) => async (req: Request, res: Response, next: NextFunction) =>  {
-    
     try {
         await schema.validate({
             body:req.body,
             query: req.query,
             params: req.params
-        });
+            //AbortEarly to show all messages, instead of returning on the first error found
+        }, {abortEarly: false});
         return next();
     } catch (error: any) {
-        log.error(error);
-        return res.status(400).send(error);
+        log.error(error)
+        return res.status(400).send(error.errors);
     }
 }   
--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -9,3 +9,4 @@ export const createUser =  async (input: DocumentDefinition<UserDocument>) => {
     }
 }
 
+ 
